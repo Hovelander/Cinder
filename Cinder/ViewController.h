3f033f0e99964d605c4270e2dd8706dae0f3c41e
Hovelander <smooreland@gmail.com>
2014-03-09 20:41:03 -0400

    Initial merge to sync local git with remote git.

---
+++ Cinder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cinder
+//
+//  Created by Abulafia on 3/9/14.
+//  Copyright (c) 2014 Abulafia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
